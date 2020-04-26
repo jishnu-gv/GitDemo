a3e7b176fc5f1bca52ff6dbf5b081e6437cc9e5d
ramesh <jishnu.gv@gmail.com>
2020-04-25 22:43:29 -0500

    FirstCommit

---
+++ src/test/java/Rameshacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package Rameshacademy;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+
+}
---
+++ src/test/java/Rameshacademy/RESTAPITest.java
@@ -0,0 +1,19 @@
+package Rameshacademy;
+
+public class RESTAPITest {
+	
+	public class RESTAPITest {
+
+		@Test
+		public void postJira()
+		{
+			System.out.println("postJira");
+		}
+		
+		@Test
+		public void deleteTwitter()
+		{
+			System.out.println("deleteTwitter");	
+		}
+
+}
